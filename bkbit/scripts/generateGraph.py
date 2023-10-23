a999c902976ab66e121aa8b31c77fc428b69f92d
Puja Trivedi <pujatrivedi@Pujas-MacBook-Air.local>
2023-10-23 09:51:23 -0700

    Modified generateGraph such that each jsonld graph only contains one authority and one species. Modified gfftranslator to account for changes in kbmodel (changing all references from object to id string) and changed json serialization to only include initialized attributes

--- bkbit/scripts/generateGraph.py
+++ bkbit/scripts/generateGraph.py
@@ -3,33 +3,28 @@ import glob
 import os
 import argparse
 
-def generateGraph(dir_path, output_fname):
-    # List all JSON files in the directory
+def generateGraph(dir_path):
+    # List of all JSON files in the directory
     json_files = glob.glob(os.path.join(dir_path, "*.json"))
-
-    # Initialize an empty list to store the combined dictionaries
-    combined_data = []
 
     # Iterate over each JSON file
     for file_name in json_files:
         print(f"Reading {file_name}")
         with open(file_name, "r") as file:
             data = json.load(file)
-            combined_data.extend(data)
 
-    # Create a dictionary with the @context and @graph
-    output_data = {
-        "@context": "https://raw.githubusercontent.com/atlaskb/models/main/jsonld-context-autogen/kbmodel.context.jsonld",
-        "@graph": combined_data
-    }
+        # Create a dictionary with the @context and @graph
+        output_data = {
+            "@context": "https://raw.githubusercontent.com/atlaskb/models/main/jsonld-context-autogen/kbmodel.context.jsonld",
+            "@graph": data
+        }
 
-    # Write the combined dictionaries to a new JSON file
-    with open(output_fname, "w") as outfile:
-        json.dump(output_data, outfile, indent=2)
+        # Write jsonld graph to output file
+        with open(file_name + "ld", "w") as outfile:
+            json.dump(output_data, outfile, indent=2)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Create a jsonld graph based on all the json files in a given directory.')
     parser.add_argument('--dir_path', '-d', type=str, required=True, help='Path to directory containing json files used to create jsonld graph. Note: all .json files in the directory will be used.')
-    parser.add_argument('--output_fname', '-o', type=str, required=True, help='Name of file where graph will be saved. Must have extension .jsonld')
     args = parser.parse_args()
-    generateGraph(args.dir_path, args.output_fname)
+    generateGraph(args.dir_path)
--- bkbit/scripts/gfftranslator.py
+++ bkbit/scripts/gfftranslator.py
@@ -7,7 +7,9 @@ import json
 import hashlib
 import uuid
 import urllib.parse
-from bkbit.models import kbmodel
+#from bkbit.models import kbmodel
+from bkbit.models import kbmodel220231002 as kbmodel
+#from mappings import *
 
 ## GLOBAL VARIABLES ##
 
@@ -49,7 +51,7 @@ def parse_data(df, authority, label, taxon_local_unique_id, version, gene_id_pre
             adict[apart[0]] = apart[1]
             attribute_set.add(apart[0])
 
-        print(f'SET OF KEYS: {adict.keys()}')
+        #print(f'SET OF KEYS: {adict.keys()}')
         # add as columns in the dataframe
         for x in adict :
             df_gene.loc[gindex,x] = adict[x]
@@ -123,8 +125,7 @@ def create_gene_annotation_objects(file, genomeAnnot, orgTaxon):
     genomeAnn (GenomeAnnotation): reference to genome annotation object.
 
     Returns:
-    [GeneAnnotation]: list of created GeneAnnotation objects using data from .csv file.
-    
+    [GeneAnnotation]: list of initialized GeneAnnotation objects    
     """
     with open(file=file, newline='') as csvfile:
         reader = csv.DictReader(csvfile)
@@ -133,14 +134,17 @@ def create_gene_annotation_objects(file, genomeAnnot, orgTaxon):
         if 'description' in header:
             has_description = True
         translatedannots = []
+
         for row in reader:
             myannotation = kbmodel.GeneAnnotation(id = row['gene_identifier_prefix'].upper().replace('ENE', 'ene') + ':' + row['gene_local_unique_identifier'].split('.')[0],
                                         symbol = row['name'],
                                         name = row['name'],
-                                        referenced_in = genomeAnnot,
+                                        #referenced_in = genomeAnnot,
+                                        referenced_in = genomeAnnot.id,
                                         molecular_type = kbmodel.BioType.protein_coding if row['gene_biotype'] == 'protein_coding' else kbmodel.BioType.noncoding, 
                                         source_id = row['gene_local_unique_identifier'].split('.')[0],
-                                        in_taxon = [orgTaxon], 
+                                        #in_taxon = [orgTaxon], 
+                                        in_taxon = [orgTaxon.id],
                                         in_taxon_label = taxon_scientific_name[row['taxon_unique_identifier']]
                                         )
             if has_description:
@@ -160,8 +164,8 @@ def serialize_annotation_collection(annotations, outfile):
     with open(outfile, 'w') as f:
         output_arr = []
         for g in annotations:
-            #output_arr.append(g.dict(exclude_none=True))
-            output_arr.append(g.dict())
+            output_arr.append(g.dict(exclude_none=True))
+            #output_arr.append(g.dict())
         f.write(json.dumps(output_arr, indent=2))
 
 def gff_to_gene_annotation(input_fname, data_dir, output_dir, genome_assembly_fname):
@@ -183,6 +187,10 @@ def gff_to_gene_annotation(input_fname, data_dir, output_dir, genome_assembly_fn
 
     # GENERATE GENOME ASSEMBLY OBJECTS #
     genomeAssemblyObjects = generate_genome_assembly(os.path.join(data_dir,genome_assembly_fname)) 
+
+    # SAVES ALL INITIALIZED GENE ANNOTATION OBJECTS #
+    ncbi_ga = dict()
+    ensembl_ga = dict()
 
     # ITERATE THROUGH GFF FILES #
     if not os.path.exists(output_dir):
@@ -224,24 +232,45 @@ def gff_to_gene_annotation(input_fname, data_dir, output_dir, genome_assembly_fn
             #checkSumObjects.append(curr_digest_256)
 
             # GENERATE GENOME ANNOTATION OBJECT #
-            curr_genome_annot = kbmodel.GenomeAnnotation(id = 'bican:annotation-' + row['label'].upper(), digest=[curr_digest_256], content_url = [row['url']], reference_assembly = genomeAssemblyObjects[row['assembly_local_unique_identifier'].strip()], version = row['version'], in_taxon = [curr_org_taxon], in_taxon_label = taxon_scientific_name[row['taxon_local_unique_identifier']], description = row['description'], authority = row['authority'].upper())
+            curr_genome_annot = kbmodel.GenomeAnnotation(id = 'bican:annotation-' + row['label'].upper(), 
+                                                         #digest = [curr_digest_256], 
+                                                         digest = [curr_digest_256.id], 
+                                                         content_url = [row['url']], 
+                                                         #reference_assembly = genomeAssemblyObjects[row['assembly_local_unique_identifier'].strip()],
+                                                         reference_assembly = genomeAssemblyObjects[row['assembly_local_unique_identifier'].strip()].id, 
+                                                         version = row['version'], 
+                                                         #in_taxon = [curr_org_taxon],
+                                                         in_taxon = [curr_org_taxon.id], 
+                                                         in_taxon_label = taxon_scientific_name[row['taxon_local_unique_identifier']], 
+                                                         description = row['description'], 
+                                                         authority = row['authority'].upper())
             #print(f'GenomeAnnotation object is created: {curr_genome_annot}')
             #genomeAnnotObjects.append(curr_genome_annot)
             
             # GENERATE GENE ANNOTATION OBJECT #
-            annotations = create_gene_annotation_objects(file, curr_genome_annot, curr_org_taxon)
-            #geneAnnotObjects.extend(annotations)
+            curr_gene_annot = create_gene_annotation_objects(file, curr_genome_annot, curr_org_taxon)
+            #geneAnnotObjects.extend(curr_gene_annot)
 
             # COMBINE ALL GENERATED OBJECTS RELATED TO THIS GFF FILE:
             fileObjects.append(genomeAssemblyObjects[row['assembly_local_unique_identifier'].strip()])
             fileObjects.append(curr_genome_annot)
             fileObjects.append(curr_org_taxon)
             fileObjects.append(curr_digest_256) 
-            fileObjects.extend(annotations)
+            fileObjects.extend(curr_gene_annot)
             output_ser = os.path.join(output_dir, gene_name +'.json')
+            #print(f'CURRENT GENES: {curr_gene_annot.values()}')
             serialize_annotation_collection(fileObjects, output_ser)
             print(f'Serialized GeneAnnotation objects saved here: {output_ser}')
 
+            # SAVE GENE ANNOTATION OBJECTS TO DICTIONARY #
+            # if row['authority'].upper() == 'NCBI':
+            #     print("IN NCBI")
+            #     ncbi_ga[row['taxon_local_unique_identifier']] = curr_gene_annot
+            # elif row['authority'].upper() == 'ENSEMBL':
+            #     print('IN ENSEMBL')
+            #     ensembl_ga[row['taxon_local_unique_identifier']] = curr_gene_annot
+    # return ncbi_ga, ensembl_ga
+    return None, None
 
 #--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
             
@@ -275,7 +304,12 @@ def generate_genome_assembly(file_path):
         id = row.local_unique_identifier.strip()
         if row.local_unique_identifier not in genomeAssemblyObjects:
             currOrgTaxon = kbmodel.OrganismTaxon(id = 'NCBITaxon:' + str(row.taxon_local_unique_identifier), full_name = taxon_scientific_name[str(row.taxon_local_unique_identifier)], name = taxon_common_name[str(row.taxon_local_unique_identifier)],  iri = prefix_map['NCBITaxon'] + str(row.taxon_local_unique_identifier))
-            currGenomeAssem = kbmodel.GenomeAssembly(id = "NCBIAssembly:" + id, in_taxon = [currOrgTaxon], in_taxon_label = taxon_scientific_name[str(row.taxon_local_unique_identifier)],  version = row.version, name = row.label)
+            currGenomeAssem = kbmodel.GenomeAssembly(id = "NCBIAssembly:" + id, 
+                                                     #in_taxon = [currOrgTaxon], 
+                                                     in_taxon = [currOrgTaxon.id], 
+                                                     in_taxon_label = taxon_scientific_name[str(row.taxon_local_unique_identifier)],
+                                                     version = str(row.version), 
+                                                     name = row.label)
             if str(row.strain) != 'nan':
                 currGenomeAssem.strain = row.strain
             genomeAssemblyObjects[id] = currGenomeAssem
