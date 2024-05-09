2e5a5165552a6490950585431aae91053a62e6f5
Puja Trivedi <xspuja@gmail.com>
2024-05-08 19:33:18 -0700

    converted a few of the functions into @staticmethod and @classmethod to make it easier to test them

--- bkbit/scripts/specimen_portal_translator.py
+++ bkbit/scripts/specimen_portal_translator.py
@@ -11,7 +11,7 @@ INFO_URL_SUFFIX = 'info?id='
 ANCESTORS_URL_SUFFIX = 'ancestors?id='
 PARENTS_URL_SUFFIX = 'parents?id='
 NHASH_ONLY_SUFFIX = '&nhash_only='
-CATEGORY_TO_CLASS = {'librarypool': pb.LibraryPool, 
+CATEGORY_TO_CLASS = {'librarypool': pb.LibraryPool,
                      'libraryaliquot': pb.LibraryAliquot,
                      'library': pb.Library,
                      'amplifiedcdna': pb.AmplifiedCdna,
@@ -22,12 +22,8 @@ CATEGORY_TO_CLASS = {'librarypool': pb.LibraryPool,
                      'donor': pb.Donor}
 
 class SpecimenPortal:
-    def __init__(self, jwt_token, bican_to_nimp_file_path):
-        self.headers = {'Authorization': f'Bearer {jwt_token}'}
-        self.generated_objects = {}
-        self.bican_to_nimp_mapping, self.bican_slots_per_class = self.create_bican_to_nimp_mapping(bican_to_nimp_file_path)
-    
-    def create_bican_to_nimp_mapping(self, csv_file):
+    @staticmethod
+    def create_bican_to_nimp_mapping(csv_file):
         """
         Creates a mapping dictionary from a CSV file, where the keys are 'LinkML Slot or Attribute Name'
         and the values are 'NIMP Variable Name'.
@@ -38,19 +34,32 @@ class SpecimenPortal:
         Returns:
             dict: A dictionary mapping 'LinkML Slot or Attribute Name' to 'NIMP Variable Name'.
         """
-        mapping = {}
+        bican_to_nimp_attribute_mapping = {}
+        nimp_to_bican_class_mapping = {}
         bican_slots_per_class = defaultdict(set)
+
+
         with open(csv_file, 'r', encoding='utf-8') as file:
             reader = csv.DictReader(file)
             for row in reader:
-                bican_name = row['SubGroup/LinkML Class Name'].lower() + '_' + row['LinkML Slot or Attribute Name'].lower() #! standardize the key
+                bican_name = row['SubGroup/LinkML Class Name'].lower() + '_' + row['LinkML Slot or Attribute Name'].lower() 
                 nimp_name = row['NIMP Variable Name']
-                mapping[bican_name] = nimp_name
-                bican_slots_per_class[row['SubGroup/LinkML Class Name'].lower()].add(row['LinkML Slot or Attribute Name'].lower()) #! standardize the key and value
-        return mapping, bican_slots_per_class
+                bican_to_nimp_attribute_mapping[bican_name] = nimp_name
+                nimp_to_bican_class_mapping[row['NIMP Category'].replace(' ', '').lower()] = row['SubGroup/LinkML Class Name'].lower()
+                bican_slots_per_class[row['SubGroup/LinkML Class Name'].lower()].add(row['LinkML Slot or Attribute Name'].lower()) 
+        return bican_to_nimp_attribute_mapping, nimp_to_bican_class_mapping, bican_slots_per_class
 
-   
-    def get_data(self, nhash_id):
+    bican_to_nimp_file_path = '../scripts/bican_to_nimp_slots.csv'
+    bican_to_nimp_attribute_mapping, nimp_to_bican_class_mapping, bican_slots_per_class = create_bican_to_nimp_mapping(bican_to_nimp_file_path)
+    
+    def __init__(self, jwt_token):
+        #self.headers = {'Authorization': f'Bearer {jwt_token}'}
+        self.jwt_token = jwt_token
+        self.generated_objects = {}
+    
+    
+    @staticmethod
+    def get_data(nhash_id, jwt_token):
         '''
         Retrieve information of any record with a NHash ID in the system. 
 
@@ -63,39 +72,34 @@ class SpecimenPortal:
         Raises:
         - requests.exceptions.HTTPError: If there is an error retrieving the data.
         '''
-        
-        response = requests.get(f'{API_URL_PREFIX}{INFO_URL_SUFFIX}{nhash_id}', headers=self.headers) #TODO: add timeout
+        headers = {'Authorization': f'Bearer {jwt_token}'}
+        response = requests.get(f'{API_URL_PREFIX}{INFO_URL_SUFFIX}{nhash_id}', headers=headers) #TODO: add timeout
         if response.status_code == 200:
             return response.json()
         
         raise requests.exceptions.HTTPError(f'Error getting data for NHash ID = {nhash_id}. Status Code: {response.status_code}')
 
-    def get_ancestors(self, nhash_id, nhash_only=True, depth=1):
+    @staticmethod
+    def get_ancestors(nhash_id, jwt_token, nhash_only=True, depth=1):
         '''
         Retrieve information of all ancestors of a record with a NHash ID. 
 
         '''
-        response = requests.get(f'{API_URL_PREFIX}{ANCESTORS_URL_SUFFIX}{nhash_id}{NHASH_ONLY_SUFFIX}{nhash_only}', headers=self.headers) #TODO: add timeout
+        headers = {'Authorization': f'Bearer {jwt_token}'}
+
+        response = requests.get(f'{API_URL_PREFIX}{ANCESTORS_URL_SUFFIX}{nhash_id}{NHASH_ONLY_SUFFIX}{nhash_only}', headers=headers) #TODO: add timeout
         if response.status_code == 200:
             return response.json()
         
         raise requests.exceptions.HTTPError(f'Error getting data for NHash ID = {nhash_id}. Status Code: {response.status_code}')
 
-    def get_parent(self, nhash_id):
-        '''
-        Retrieve information of a record’s parents with a NHash ID. 
-        '''
-        response = requests.get(f'{API_URL_PREFIX}{PARENTS_URL_SUFFIX}{nhash_id}', headers=self.headers) #TODO: add timeout
-        if response.status_code == 200:
-            return response.json()
-        
-        raise requests.exceptions.HTTPError(f'Error getting data for NHash ID = {nhash_id}. Status Code: {response.status_code}')
-
-    def generate_bican_object(self, nhash_id:str, was_derived_from: list[str]=None):
-        #TODO: 1. Add name to bican_to_nimp_mapping
+    @classmethod
+    def generate_bican_object(cls, data, was_derived_from: list[str]=None):
         #TODO: 2. check if NHash ID is not found
-        data = self.get_data(nhash_id).get('data')
+        #data = self.get_data(nhash_id).get('data')
+        nhash_id = data.get('id')
         category = data.get('category').replace(' ', '').lower()
+        bican_category = cls.nimp_to_bican_class_mapping.get(category)
         bican_class = CATEGORY_TO_CLASS.get(category)
         bican_object = bican_class(id='NIMP:'+ nhash_id)
         # handle was_derived_from attribute. type of this attribute can either be Optional[str] or Optional[List[str]]
@@ -103,42 +107,52 @@ class SpecimenPortal:
             bican_object.was_derived_from = [f"NIMP:{item}" for item in was_derived_from]
         else:
             bican_object.was_derived_from = f"NIMP:{was_derived_from[0]}" if was_derived_from else None
-        class_attributes = self.bican_slots_per_class.get(category) 
+        class_attributes = SpecimenPortal.bican_slots_per_class.get(bican_category)
         if class_attributes is not None:
-            for attribute in self.bican_slots_per_class.get(category):   
-                if (category + '_' + attribute) in self.bican_to_nimp_mapping:
+            for attribute in class_attributes:   
+                if (bican_category + '_' + attribute) in SpecimenPortal.bican_to_nimp_attribute_mapping:
                     bican_attribute_type = bican_class.__annotations__[attribute]
-                    value = data.get('record').get(self.bican_to_nimp_mapping[category + '_' + attribute])
+                    value = data.get('record').get(SpecimenPortal.bican_to_nimp_attribute_mapping[bican_category + '_' + attribute])
                     if value is not None and type(value) != bican_attribute_type:
-                        if 'str' in bican_attribute_type:
-                            value = str(value)
+                        if 'AmplifiedCdnaRnaAmplificationPassFail' in bican_attribute_type:
+                            value = SpecimenPortal.__check_valueset_membership('AmplifiedCdnaRnaAmplificationPassFail', value)
+                        elif 'BarcodedCellSampleTechnique' in bican_attribute_type:
+                            value = SpecimenPortal.__check_valueset_membership('BarcodedCellSampleTechnique', value)
+                        elif 'DissociatedCellSampleCellPrepType' in bican_attribute_type:
+                            value = SpecimenPortal.__check_valueset_membership('DissociatedCellSampleCellPrepType', value)
+                        elif 'DissociatedCellSampleCellLabelBarcode' in bican_attribute_type:
+                            value = SpecimenPortal.__check_valueset_membership('DissociatedCellSampleCellLabelBarcode', value)
+                        elif 'LibraryTechnique' in bican_attribute_type:
+                            value = SpecimenPortal.__check_valueset_membership('LibraryTechnique', value)
+                        elif 'LibraryPrepPassFail' in bican_attribute_type:
+                            value = SpecimenPortal.__check_valueset_membership('LibraryPrepPassFail', value)
+                        elif 'LibraryR1R2Index' in bican_attribute_type:
+                            value = SpecimenPortal.__check_valueset_membership('LibraryR1R2Index', value)
+                        elif 'Sex' in bican_attribute_type:
+                            value = SpecimenPortal.__check_valueset_membership('Sex', value)
+                        elif 'AgeAtDeathReferencePoint' in bican_attribute_type:
+                            value = SpecimenPortal.__check_valueset_membership('AgeAtDeathReferencePoint', value)
+                        elif 'AgeAtDeathUnit' in bican_attribute_type:
+                            value = SpecimenPortal.__check_valueset_membership('AgeAtDeathUnit', value)
+                        elif 'str' in bican_attribute_type:
+                            if 'List' in bican_attribute_type:
+                                pass
+                            else:
+                                value = str(value)
                         elif 'int' in bican_attribute_type:
                             value = int(float(value))
                         elif 'float' in bican_attribute_type:
                             value = float(value)
                         elif 'bool' in bican_attribute_type:
                             value = bool(value)
-                        elif 'AmplifiedCdnaRnaAmplificationPassFail' in bican_attribute_type:
-                            value = self.__check_valueset_membership('AmplifiedCdnaRnaAmplificationPassFail', value)
-                        elif 'BarcodedCellSampleTechnique' in bican_attribute_type:
-                            value = self.__check_valueset_membership('BarcodedCellSampleTechnique', value)
-                        elif 'DissociatedCellSampleCellPrepType' in bican_attribute_type:
-                            value = self.__check_valueset_membership('DissociatedCellSampleCellPrepType', value)
-                        elif 'DissociatedCellSampleCellLabelBarcode' in bican_attribute_type:
-                            value = self.__check_valueset_membership('DissociatedCellSampleCellLabelBarcode', value)
-                        elif 'LibraryTechnique' in bican_attribute_type:
-                            value = self.__check_valueset_membership('LibraryTechnique', value)
-                        elif 'LibraryPrepPassFail' in bican_attribute_type:
-                            value = self.__check_valueset_membership('LibraryPrepPassFail', value)
-                        elif 'LibraryR1R2Index' in bican_attribute_type:
-                            value = self.__check_valueset_membership('LibraryR1R2Index', value)
                         else:
                             #print(f'Attribute {attribute} of class {category} is of type {bican_attribute_type} which is not supported.')
                             value = None
                     bican_object.__setattr__(attribute, value)
         return bican_object
 
-    def __check_valueset_membership(self, enum_name, nimp_value):
+    @staticmethod
+    def __check_valueset_membership(enum_name, nimp_value):
         enum = pb.__dict__.get(enum_name)
         if enum is not None:
             valueset = {m.value: m for m in enum}
@@ -146,13 +160,15 @@ class SpecimenPortal:
         return None
 
     def parse_nhash_id(self, nhash_id):
-        ancestor_tree = self.get_ancestors(nhash_id).get('data')
+        ancestor_tree = SpecimenPortal.get_ancestors(nhash_id, self.jwt_token).get('data')
         stack = [nhash_id]
         while stack:
             current_nhash_id = stack.pop()
             if current_nhash_id not in self.generated_objects:
                 parents = ancestor_tree.get(current_nhash_id).get('edges').get('has_parent')
-                bican_object = self.generate_bican_object(current_nhash_id, parents)
+                #bican_object = self.generate_bican_object(current_nhash_id, parents)
+                data = SpecimenPortal.get_data(current_nhash_id, self.jwt_token).get('data')
+                bican_object = self.generate_bican_object(data, parents)
                 if bican_object is not None:
                     self.generated_objects[current_nhash_id] = bican_object   
                 if parents is not None:
@@ -195,7 +211,8 @@ class SpecimenPortal:
         pass
 
 if __name__ == '__main__':
-    temp = SpecimenPortal('eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxMTAsImV4cCI6MTcxNDE3Mzk5NH0.NZd07OqzhRImLcffPxcojEUO6W6rqxRFHrvgdPMbmg8', 'bican_to_nimp_slots.csv')
+    temp = SpecimenPortal('eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxMTAsImV4cCI6MTcxNTIxODYyNX0.fztv_nJ9sYWtrH5CZEyt2iC5vOnQPQWh50mg-7jStqk', 'bican_to_nimp_slots.csv')
     #temp.generate_bican_object('LI-DDFMNG372245')
-    temp.parse_nhash_id('LP-CVFLMQ819998')    
-    temp.serialize_to_jsonld('output_LP-CVFLMQ819998_20240426.jsonld')
+    temp.parse_nhash_id('LP-CVFLMQ819998')
+    #temp.parse_nhash_id("AC-ATDJAH472237")
+    temp.serialize_to_jsonld('output_LP-CVFLMQ819998_20240502.jsonld')
