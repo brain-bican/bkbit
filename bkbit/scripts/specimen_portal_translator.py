e1b42323e96532dc573a6f1fdd559204012ebb70
Puja Trivedi <xspuja@gmail.com>
2024-05-14 18:54:27 -0700

    cleaned up code and updated readme

--- bkbit/scripts/specimen_portal_translator.py
+++ bkbit/scripts/specimen_portal_translator.py
@@ -1,11 +1,10 @@
-import requests
-from bkbit.models import library_generation as lg
+from collections import defaultdict
 import csv
 import json
-from collections import defaultdict
+import requests
+from bkbit.models import library_generation as lg
 
-
-PURPLE_BOXES_MODEL = "pb"
+BICAN_TO_NIMP_FILE_PATH = "../utils/bican_to_nimp_slots.csv"
 API_URL_PREFIX = "https://brain-specimenportal.org/api/v1/nhash_ids/"
 INFO_URL_SUFFIX = "info?id="
 ANCESTORS_URL_SUFFIX = "ancestors?id="
@@ -33,11 +32,12 @@ class SpecimenPortal:
         Creates a mapping dictionary from a CSV file, where the keys are 'LinkML Slot or Attribute Name'
         and the values are 'NIMP Variable Name'.
 
-        Args:
+        Parameters:
             csv_file (str): The path to the CSV file.
 
         Returns:
             dict: A dictionary mapping 'LinkML Slot or Attribute Name' to 'NIMP Variable Name'.
+
         """
         bican_to_nimp_attribute_mapping = {}
         nimp_to_bican_class_mapping = {}
@@ -65,15 +65,13 @@ class SpecimenPortal:
             bican_slots_per_class,
         )
 
-    bican_to_nimp_file_path = "../scripts/bican_to_nimp_slots.csv"
     (
         bican_to_nimp_attribute_mapping,
         nimp_to_bican_class_mapping,
         bican_slots_per_class,
-    ) = create_bican_to_nimp_mapping(bican_to_nimp_file_path)
+    ) = create_bican_to_nimp_mapping(BICAN_TO_NIMP_FILE_PATH)
 
     def __init__(self, jwt_token):
-        # self.headers = {'Authorization': f'Bearer {jwt_token}'}
         self.jwt_token = jwt_token
         self.generated_objects = {}
 
@@ -83,18 +81,22 @@ class SpecimenPortal:
         Retrieve information of any record with a NHash ID in the system.
 
         Parameters:
-        - nhash_id (str): The NHash ID of the record to retrieve.
+            nhash_id (str): The NHash ID of the record to retrieve.
+            jwt_token (str): The JWT token for authentication.
 
         Returns:
-        - dict: The JSON response containing the information of the record.
+            dict: The JSON response containing the information of the record.
 
         Raises:
-        - requests.exceptions.HTTPError: If there is an error retrieving the data.
+            requests.exceptions.HTTPError: If there is an error retrieving the data.
+
         """
         headers = {"Authorization": f"Bearer {jwt_token}"}
         response = requests.get(
-            f"{API_URL_PREFIX}{INFO_URL_SUFFIX}{nhash_id}", headers=headers
-        )  # TODO: add timeout
+            f"{API_URL_PREFIX}{INFO_URL_SUFFIX}{nhash_id}",
+            headers=headers,
+            timeout=10,  # ? is this an appropriate timeout value?
+        )
         if response.status_code == 200:
             return response.json()
 
@@ -105,7 +107,19 @@ class SpecimenPortal:
     @staticmethod
     def get_ancestors(nhash_id, jwt_token, nhash_only=True, depth=1):
         """
-        Retrieve information of all ancestors of a record with a NHash ID.
+        Retrieve information of all ancestors of a record with the given NHash ID.
+
+        Parameters:
+            nhash_id (str): The NHash ID of the record.
+            jwt_token (str): The JWT token for authentication.
+            nhash_only (bool): Flag indicating whether to retrieve only NHash IDs or complete record information. Default is True.
+            depth (int): The depth of ancestors to retrieve. Default is 1.
+
+        Returns:
+            dict: The JSON response containing information of all ancestors.
+
+        Raises:
+            requests.exceptions.HTTPError: If there is an error getting data for the NHash ID.
 
         """
         headers = {"Authorization": f"Bearer {jwt_token}"}
@@ -113,7 +127,8 @@ class SpecimenPortal:
         response = requests.get(
             f"{API_URL_PREFIX}{ANCESTORS_URL_SUFFIX}{nhash_id}{NHASH_ONLY_SUFFIX}{nhash_only}",
             headers=headers,
-        )  # TODO: add timeout
+            timeout=10,  # This is an appropriate timeout value.
+        )
         if response.status_code == 200:
             return response.json()
 
@@ -123,12 +138,24 @@ class SpecimenPortal:
 
     @classmethod
     def generate_bican_object(cls, data, was_derived_from: list[str] = None):
-        # TODO: 2. check if NHash ID is not found
-        # data = self.get_data(nhash_id).get('data')
+        """
+        Generate a Bican object based on the provided data.
+
+        Parameters:
+            data (dict): The data retrieved from the NIMP portal.
+            was_derived_from (list): A list of parent NHash IDs.
+
+        Returns:
+            The generated Bican object.
+
+        Raises:
+            None.
+
+        """
+        # TODO: check if NHash ID is not found
         nhash_id = data.get("id")
         category = data.get("category").replace(" ", "").lower()
         bican_category = cls.nimp_to_bican_class_mapping.get(category)
-        # print(f'Category: {category}, Bican Category: {bican_category}')
         if bican_category is None:
             return None
         bican_class = CATEGORY_TO_CLASS.get(category)
@@ -161,7 +188,7 @@ class SpecimenPortal:
                         ):
                             value = SpecimenPortal.__check_valueset_membership(
                                 "AmplifiedCdnaRnaAmplificationPassFail", value
-                            )
+                            ) 
                         elif "BarcodedCellSampleTechnique" in bican_attribute_type:
                             value = SpecimenPortal.__check_valueset_membership(
                                 "BarcodedCellSampleTechnique", value
@@ -215,13 +242,22 @@ class SpecimenPortal:
                         elif "bool" in bican_attribute_type:
                             value = bool(value)
                         else:
-                            # print(f'Attribute {attribute} of class {category} is of type {bican_attribute_type} which is not supported.')
                             value = None
                     bican_object.__setattr__(attribute, value)
         return bican_object
 
     @staticmethod
     def __check_valueset_membership(enum_name, nimp_value):
+        """
+        Check if the given value belongs to the specified enum.
+
+        Parameters:
+            enum_name (str): The name of the enum to check.
+            nimp_value: The value to check for membership in the enum.
+
+        Returns:
+            The enum member if the value belongs to the enum, None otherwise.
+        """
         enum = lg.__dict__.get(enum_name)
         if enum is not None:
             valueset = {m.value: m for m in enum}
@@ -239,11 +275,9 @@ class SpecimenPortal:
                 parents = (
                     ancestor_tree.get(current_nhash_id).get("edges").get("has_parent")
                 )
-                # bican_object = self.generate_bican_object(current_nhash_id, parents)
                 data = SpecimenPortal.get_data(current_nhash_id, self.jwt_token).get(
                     "data"
                 )
-                # print(f'Processing NHash ID: {current_nhash_id}')
                 bican_object = self.generate_bican_object(data, parents)
                 if bican_object is not None:
                     self.generated_objects[current_nhash_id] = bican_object
@@ -268,28 +302,30 @@ class SpecimenPortal:
                 # data.append(obj.to_dict(exclude_none=exclude_none, exclude_unset=exclude_unset))
                 data.append(obj.__dict__)
             output_data = {
-                "@context": "https://raw.githubusercontent.com/djarecka/models_tests/main/purple_boxes.context.json",
+                "@context": "https://raw.githubusercontent.com/brain-bican/models/main/jsonld-context-autogen/library_generation.context.jsonld",
                 "@graph": data,
             }
             f.write(json.dumps(output_data, indent=2))
 
 
 if __name__ == "__main__":
-    # temp = SpecimenPortal('eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxMTAsImV4cCI6MTcxNTI0NDgxNX0.-bB0KPxN93WGTFyWyGFXNv5mwqyPr2Cizz8-EtkTMxA')
-    # #temp.generate_bican_object('LI-DDFMNG372245')
-    # temp.parse_nhash_id('LP-CVFLMQ819998')
-    # #temp.parse_nhash_id("AC-ATDJAH472237")
-    # temp.serialize_to_jsonld('output_LP-CVFLMQ819998_20240508.jsonld')
+    # sp = SpecimenPortal(
+    #     "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxMTAsImV4cCI6MTcxNTc1NzUzN30.2CsWyCHwtOAd4NnOUMinhgtTk86z0ydh0T5__rfh824"
+    # )
+    # sp.parse_nhash_id('AC-ATDJAH472237')
 
+    token = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxMTAsImV4cCI6MTcxNTc1NzUzN30.2CsWyCHwtOAd4NnOUMinhgtTk86z0ydh0T5__rfh824"
     sp = SpecimenPortal(
-        "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxMTAsImV4cCI6MTcxNTI5MzcwMX0.xquqUIIFsPynRc25SfU2GMqpg2gatSfldyYlqRhql8k"
+        token
     )
+    LIMIT = 10
     with open("example_library_pool_data.csv", "r", encoding="utf-8") as file:
         reader = csv.DictReader(file)
-        count = 1
+        row_number = 1
         for row in reader:
             print(f'Processing LP: {row["NHash ID"]}')
             sp.parse_nhash_id(row["NHash ID"])
             sp.serialize_to_jsonld("output_" + row["NHash ID"] + ".jsonld")
-            if count > 10:
+            if row_number == LIMIT:
                 break
+            row_number += 1
