b256e3462b3decf1b1556f0ada53be684119ac5f
Dorota Jarecka <djarecka@gmail.com>
2024-06-01 23:06:00 -0400

    adding fixing parts; saving fixed tsv files in a separate directory

--- bkbit/scripts/sheets_converter.py
+++ bkbit/scripts/sheets_converter.py
@@ -1,5 +1,6 @@
 import os, sys
-import yaml
+import csv, yaml
+from io import StringIO
 import click
 from pathlib import Path
 
@@ -8,72 +9,82 @@ from linkml_runtime.utils.schema_as_dict import schema_as_dict
 from schemasheets import schemamaker as sm
 
 
-def changed_tsv_files(tsv_files):
+def fix_tsv_files(tsv_files):
     """
     Check if the tsv files have changed
     :param tsv_files:
     :return:
     """
-
+    tsv_file_fixed_list = []
+    dir_fixed = Path(tsv_files[0]).parent / "fixed_sheets"
+    dir_fixed.mkdir(exist_ok=True)
     for tsv_file in list(tsv_files):
         modified_rows = []
         with open(tsv_file, 'r', newline='') as file:
-            tsv_reader = csv.reader(file, delimiter='\t')
-            for ii, row in enumerate(tsv_reader):
-                if ii == 1:
-                    columns_to_change = []
-                    for jj, col in enumerate(row):
-                        if "mapping" in col.lower():
-                            columns_to_change.append(jj)
-                if ii > 1:
-                    for jj in columns_to_change:
-                        if jj > len(row)-1: breakpoint()
-                        if row[jj]:
-                            row[jj] = row[jj].replace(" ", "%20")
-                modified_rows.append(row)
+            # cleaned of any ^M characters
+            content = file.read().replace('\r', '')
+        # convert the cleaned content back to a file-like object
+        data = StringIO(content)
+        # read the file-like object as a csv file
+        tsv_reader = csv.reader(data, delimiter='\t')
+        for ii, row in enumerate(tsv_reader):
+            if ii == 1:
+                columns_to_change = []
+                for jj, col in enumerate(row):
+                    if "mapping" in col.lower():
+                        columns_to_change.append(jj)
+            if ii > 1:
+                for jj in columns_to_change:
+                    if jj > len(row)-1: breakpoint()
+                    if row[jj]:
+                        row[jj] = row[jj].replace(" ", "%20")
+            modified_rows.append(row)
 
-        with open(tsv_file, 'w', newline='') as file:
+        tsv_file_fixed = dir_fixed / Path(tsv_file).name
+        tsv_file_fixed_list.append(str(tsv_file_fixed))
+        with open(tsv_file_fixed, 'w', newline='') as file:
             tsv_writer = csv.writer(file, delimiter='\t')
             tsv_writer.writerows(modified_rows)
+    return tsv_file_fixed_list
 
 
 
-def bican_fix(schema: SchemaDefinition, template_yaml) -> SchemaDefinition:
+def bican_fix(schema: SchemaDefinition) -> SchemaDefinition:
     """
     Apply BICAN specific fixes to the schema
     :param schema:
     :return:
     """
-
-
+    # fixing values for categories
     for nm, cl in schema.classes.items():
         cl.slot_usage["category"] = SlotDefinition(name="category", pattern=r"^bican:[A-Z][A-Za-z]+$")
+        # not needed anymore
+        # if "name_pr" in cl.slots:
+        #     cl.slots["name"] = cl.slots.pop("name_pr")
+        # if "slot_usage" in cl and "name_pr" in cl["slot_usage"]:
+        #     cl.slot_usage["name"] = cl.slot_usage.pop("name_pr")
 
+    # removing names from the slots
+    if "name" in schema.slots:
+        del schema.slots["name"]
+    # removing empty subsets that are from imported biolink schema
+    biolink_subsets = []
+    for nm, subs in schema.subsets.items():
+        if not subs.description and not subs.from_schema:
+            biolink_subsets.append(nm)
+    for nm in biolink_subsets:
+        del schema.subsets[nm]
+    return schema
+
+
+def adding_template(schema:SchemaDefinition, template_yaml) -> SchemaDefinition:
     with (template_yaml).open() as file:
         classes_base = yaml.safe_load(file)
         for key, val in classes_base["classes"].items():
             schema.classes[key] = val
         for key, val in classes_base["slots"].items():
             schema.slots[key] = val
-
     return schema
-
-def fixing_name_keys_yaml(output_file):
-    with Path(output_file).open() as file:
-        model = yaml.safe_load(file)
-
-    for key, cl in model["classes"].items():
-        #breakpoint()
-        if "name_pr" in cl["slots"]:
-            cl["slots"] = ["name" if el == "name_pr" else el for el in cl["slots"]]
-        if "slot_usage" in cl and "name_pr" in cl["slot_usage"]:
-            cl["slot_usage"]["name"] = cl["slot_usage"].pop("name_pr")
-
-    if "name_pr" in model["slots"]:
-        model["slots"]["name"] = model["slots"].pop("name_pr")
-
-    with Path(output_file).open("w") as file:
-        yaml.safe_dump(model, file, sort_keys=False)
 
 
 @click.command()
@@ -81,6 +92,10 @@ def fixing_name_keys_yaml(output_file):
               type=click.File(mode="w"),
               default=sys.stdout,
               help="output file")
+@click.option('-t', '--template',
+              type=click.Path(exists=True),
+              default=None,
+              help="template file")
 @click.option("--fix_tsv/--no-fix_tsv",
               default=False,
               show_default=True,
@@ -90,30 +105,34 @@ def fixing_name_keys_yaml(output_file):
               show_default=True,
               help="Auto-repair schema")
 @click.option("--fix_bican_model/--no-fix_bican_model",
-              default=True,
+              default=False,
               show_default=True,
               help="Auto-repair specifically for bican yaml model")
 @click.argument('tsv_files', nargs=-1)
-def generateSchema(tsv_files, output, fix_tsv, repair, fix_bican_model):
+def generateSchema(tsv_files, output, fix_tsv, repair, fix_bican_model, template):
     schema_maker = sm.SchemaMaker()
 
+    if not template:
+        # checking default name of template
+        if (Path(tsv_files[0]).parent / "classes_base.yaml").exists():
+            template = Path(tsv_files[0]).parent / "classes_base.yaml"
+
     if fix_tsv:
-        changed_tsv_files(list(tsv_files))
+        tsv_files = fix_tsv_files(list(tsv_files))
 
     schema = schema_maker.create_schema(list(tsv_files))
     if repair:
         schema = schema_maker.repair_schema(schema)
 
     if fix_bican_model:
-        schema = bican_fix(schema, template_yaml=Path(tsv_files[0]).parent / "classes_base.yaml")
+        schema = bican_fix(schema)
+
+    if template:
+        schema = adding_template(schema, template_yaml=template)
 
     schema_dict = schema_as_dict(schema)
     output.write(yaml.dump(schema_dict, sort_keys=False))
     output.close()
-
-    if fix_bican_model:
-         fixing_name_keys_yaml(Path(output.name))
-
 
 
 if __name__ == '__main__':
