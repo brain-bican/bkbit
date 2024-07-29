1c1eb0ab1ab9d6909d34f8ebb6ebfb9a138c66a9
Puja Trivedi <xspuja@gmail.com>
2024-07-28 21:42:46 -0700

    added url parsing to help reduce number of cli arguments

--- bkbit/data_translators/genome_annotation_translator.py
+++ bkbit/data_translators/genome_annotation_translator.py
@@ -5,6 +5,7 @@ import uuid
 import logging
 import urllib
 import urllib.request
+from urllib.parse import urlparse
 import os
 import json
 from datetime import datetime
@@ -13,6 +14,8 @@ import subprocess
 import gzip
 from tqdm import tqdm
 import click
+
+
 from bkbit.models import genome_annotation as ga
 
 
@@ -47,6 +50,28 @@ TAXON_SCIENTIFIC_NAME = {
     "9407": "Rousettus aegyptiacus",
     "9598": "Pan troglodytes"
 }
+
+SCIENTIFIC_NAME_TO_TAXONID = {
+    "Homo sapiens": "9606",
+    "Mus musculus": "10090",
+    "Macaca mulatta": "9544",
+    "Callithrix jacchus": "9483",
+    "Chlorocebus sabaeus": "60711",
+    "Dasypus novemcinctus": "9361",
+    "Felis catus": "9685",
+    "Mustela putorius furo": "9669",
+    "Otolemur garnettii": "30611",
+    "Gorilla gorilla": "9593",
+    "Monodelphis domestica": "13616",
+    "Sus scrofa": "9823",
+    "Oryctolagus cuniculus": "9986",
+    "Rattus norvegicus": "10116",
+    "Saimiri boliviensis": "27679",
+    "Tupaia belangeri chinensis": "246437",
+    "Rousettus aegyptiacus": "9407",
+    "Pan troglodytes": "9598"
+}
+
 TAXON_COMMON_NAME = {
     "9606": "human",
     "10090": "mouse",
@@ -67,6 +92,7 @@ TAXON_COMMON_NAME = {
     "9407": "egyptian fruit bat",
     "9598": "chimpanzee"
 }
+
 PREFIX_MAP = {
     "NCBITaxon": "http://purl.obolibrary.org/obo/NCBITaxon_",
     "NCBIGene": "http://identifiers.org/ncbigene/",
@@ -82,82 +108,189 @@ GENOME_ANNOTATION_DESCRIPTION_FORMAT = (
     "{authority} {taxon_scientific_name} Annotation Release {genome_version}"
 )
 DEFAULT_FEATURE_FILTER = ("gene", "pseudogene", "ncRNA_gene")
-DEFAULT_HASH = ("SHA256", "MD5")
+DEFAULT_HASH = ["MD5"]
 
 
 class Gff3:
     def __init__(
         self,
         content_url,
-        taxon_id,
-        assembly_id,
-        assembly_version,
-        assembly_label,
-        genome_label: str,
-        genome_version: str,
-        genome_authority: str,
-        hash_functions: tuple[str] = DEFAULT_HASH,
+        assembly_accession=None,
         assembly_strain=None,
+        hash_functions=DEFAULT_HASH,
     ):
         """
         Initializes an instance of the GFFTranslator class.
 
         Parameters:
         - content_url (str): The URL of the GFF file.
-        - taxon_id (int): The taxon ID of the organism.
         - assembly_id (str): The ID of the genome assembly.
-        - assembly_version (str): The version of the genome assembly.
-        - assembly_label (str): The label of the genome assembly.
-        - genome_label (str): The label of the genome.
-        - genome_version (str): The version of the genome.
-        - genome_authority (str): The authority responsible for the genome.
-        - hash_functions (tuple[str], optional): A list of hash functions to use for generating checksums. Defaults to ("SHA256", "MD5").
         - assembly_strain (str, optional): The strain of the genome assembly. Defaults to None.
+        - hash_functions (tuple[str]): A tuple of hash functions to use for generating checksums. Defaults to ('MD5').
         """
         self.logger = logger
         self.content_url = content_url
-        self.gff_file = self.__download_gff_file()
-        self.authority = self.assign_authority_type(genome_authority)
+        
+        ## STEP 1: Parse the content URL to get metadata
+        # Parse content_url to get metadata
+        url_metadata = self.parse_url()
+        print(f'URL Metadata: {url_metadata}')
+        if url_metadata is None:
+            logger.critical(
+                "The provided content URL is not supported. Please provide a valid URL."
+            )
+            raise ValueError(
+                "The provided content URL is not supported. Please provide a valid URL."
+            )
+        
+        # Define variables to store metadata
+        taxon_id, assembly_id, assembly_version, assembly_label, genome_label, genome_version = None, None, None, None, None, None
+
+        # Assign the authority type
+        self.authority = url_metadata.get("authority")
+
+        # Assign the taxon_id and assembly_id based on the authority
+        if self.authority.value == ga.AuthorityType.NCBI.value:
+            taxon_id = url_metadata.get("taxonid")
+            assembly_id = url_metadata.get("assembly_accession")
+        elif self.authority.value == ga.AuthorityType.ENSEMBL.value:
+            self.taxon_id = SCIENTIFIC_NAME_TO_TAXONID.get(url_metadata.get("species").replace("_", " "))
+            if assembly_accession is None:
+                logger.critical(
+                    "The assembly ID is required for Ensembl URLs. Please provide the assembly ID."
+                )
+                raise ValueError(
+                    "The assembly ID is required for Ensembl URLs. Please provide the assembly ID."
+                )
+            assembly_id = assembly_accession
+
+        # Assign assembly_version, assembly_label, genome_version, and genome_label
+        assembly_version = assembly_id.split(".").get(1, None)
+        assembly_label = url_metadata.get("assembly_name")
+        genome_version = url_metadata.get("release_version")
+        genome_label = self.authority.value + "-" + taxon_id + "-" + genome_version
+
+        ## STEP 2: Download the GFF file 
+        # Download the GFF file
+        self.gff_file, hash_values = self.__download_gff_file()
+
+        ## STEP 3: Generate the organism taxon, genome assembly, checksums, and genome annotation objects
+        # Generate the organism taxon object
         self.organism_taxon = self.generate_organism_taxon(taxon_id)
         self.genome_assembly = self.generate_genome_assembly(
             assembly_id, assembly_version, assembly_label, assembly_strain
         )
-        self.checksums = self.generate_digest(hash_functions)
+        self.checksums = self.generate_digest(hash_values, hash_functions)
         self.genome_annotation = self.generate_genome_annotation(
             genome_label, genome_version
         )
+        
         self.gene_annotations = {}
 
     def __download_gff_file(self):
-        """
-        Downloads a GFF file from the specified content URL, decompresses it, and returns the path to the temporary file.
+        # """
+        # Downloads a GFF file from the specified content URL, decompresses it, and returns the path to the temporary file.
 
-        Returns:
-            str: The path to the temporary file containing the decompressed GFF data.
-        """
-        # Request the file and get its size
+        # Returns:
+        #     str: The path to the temporary file containing the decompressed GFF data.
+        # """
+        # # Request the file and get its size
+        # response = urllib.request.urlopen(self.content_url)
+        # total_size = int(response.headers.get('content-length', 0))
+        # block_size = 1024  # 1 Kilobyte
+
+        # # Create a temporary file for the gzip data
+        # with tempfile.NamedTemporaryFile(suffix=".gz", delete=False) as f_gzip:
+        #     gzip_file_path = f_gzip.name
+            
+        #     # Create a progress bar
+        #     progress_bar = tqdm(total=total_size, unit='iB', unit_scale=True, desc="Downloading GFF file")
+
+        #     # Read the file in chunks and write to the temporary file
+        #     while True:
+        #         data = response.read(block_size)
+        #         if not data:
+        #             break
+        #         f_gzip.write(data)
+        #         progress_bar.update(len(data))
+
+        #     progress_bar.close()
+
+        # return gzip_file_path
         response = urllib.request.urlopen(self.content_url)
         total_size = int(response.headers.get('content-length', 0))
         block_size = 1024  # 1 Kilobyte
 
+        # Create hash objects
+        md5_hash = hashlib.md5()
+        sha256_hash = hashlib.sha256()
+        sha1_hash = hashlib.sha1()
+
         # Create a temporary file for the gzip data
         with tempfile.NamedTemporaryFile(suffix=".gz", delete=False) as f_gzip:
             gzip_file_path = f_gzip.name
-            
+
             # Create a progress bar
             progress_bar = tqdm(total=total_size, unit='iB', unit_scale=True, desc="Downloading GFF file")
 
-            # Read the file in chunks and write to the temporary file
+            # Read the file in chunks, write to the temporary file, and update the hash
             while True:
                 data = response.read(block_size)
                 if not data:
                     break
                 f_gzip.write(data)
+                md5_hash.update(data)
+                sha256_hash.update(data)
+                sha1_hash.update(data)
                 progress_bar.update(len(data))
 
             progress_bar.close()
 
-        return gzip_file_path
+        # Return the path to the temporary file and the md5 hash
+        return gzip_file_path, {"MD5": md5_hash.hexdigest(), "SHA256": sha256_hash.hexdigest(), "SHA1":sha1_hash.hexdigest()}
+    
+    def parse_url(self):
+        # NCBI : [assembly accession.version]_[assembly name]_[content type].[optional format]
+        # ENSEMBL :  <species>.<assembly>.<_version>.gff3.gz -> organism full name, assembly name, genome version
+        # Define regex patterns for NCBI and Ensembl URLs
+        ncbi_pattern = r'/genomes/all/annotation_releases/(\d+)(?:/(\d+))?/(GCF_\d+\.\d+)[_-]([^/]+)/(GCF_\d+\.\d+)[_-]([^/]+)_genomic\.gff\.gz'
+        ensembl_pattern = r'/([^/]+)\.([^/]+)\.(\d+)\.gff3\.gz$'
+        
+        # Parse the URL to get the path
+        parsed_url = urlparse(self.content_url)
+        print(f'Parsed URL: {parsed_url}')
+        path = parsed_url.path
+        
+        # Determine if the URL is from NCBI or Ensembl and extract information
+        if 'ncbi' in parsed_url.netloc:
+            print("Parsing NCBI URL")
+            ncbi_match = re.search(ncbi_pattern, path)
+            print(ncbi_match)
+            if ncbi_match:
+                print("MATCH NCBI FOUND")
+                return {
+                    "authority": ga.AuthorityType.NCBI,
+                    "taxonid": ncbi_match.group(1),
+                    "release_version": ncbi_match.group(2) if ncbi_match.group(2) else ncbi_match.group(4),
+                    "assembly_accession": ncbi_match.group(3),
+                    "assembly_name": ncbi_match.group(6),
+                }
+        
+        elif 'ensembl' in parsed_url.netloc:
+            ensembl_match = re.search(ensembl_pattern, path)
+            print(ensembl_match)
+            print("Parsing Ensembl URL")
+            if ensembl_match:
+                print("MATCH ENSEMBL FOUND")
+                return {
+                    "authority": ga.AuthorityType.ENSEMBL,
+                    "species": ensembl_match.group(1),
+                    "assembly_name": ensembl_match.group(2),
+                    "release_version": ensembl_match.group(3)
+                }
+        
+        # If no match is found, return None
+        return None
 
     def generate_organism_taxon(self, taxon_id: str):
         """
@@ -261,7 +394,8 @@ class Gff3:
 
     def generate_digest(
         self,
-        hash_functions: tuple[str] = DEFAULT_HASH,
+        hash_values: dict,
+        hash_functions: tuple[str] = DEFAULT_HASH, 
     ) -> list[ga.Checksum]:
         """
         Generates checksum digests for the GFF file using the specified hash functions.
@@ -277,11 +411,7 @@ class Gff3:
 
         """
         self.logger.debug("Generating checksum digests")
-        gff_data = open(
-            self.gff_file, "rb"
-        ).read()  # TODO: Modify this to read the file in chunks
         checksums = []
-
         for hash_type in hash_functions:
             self.logger.debug("Generating checksum for %s", hash_type)
             # Generate a UUID version 4
@@ -291,30 +421,31 @@ class Gff3:
             urn = f"urn:uuid:{uuid_value}"
             hash_type = hash_type.strip().upper()
             # Create a Checksum object
-            if hash_type == "SHA256":
+            if hash_type == ga.DigestType.SHA256.name:
                 self.logger.debug("Generating SHA256 digest")
-                digest = hashlib.sha256(gff_data).hexdigest()
                 checksums.append(
                     ga.Checksum(
                         id=urn,
                         checksum_algorithm=ga.DigestType.SHA256,
-                        value=digest,
+                        value=hash_values.get("SHA256")
                     )
                 )
-            elif hash_type == "MD5":
+            elif hash_type == ga.DigestType.MD5.name:
                 self.logger.debug("Generating MD5 digest")
-                digest = hashlib.md5(gff_data).hexdigest()
                 checksums.append(
                     ga.Checksum(
-                        id=urn, checksum_algorithm=ga.DigestType.MD5, value=digest
+                        id=urn, 
+                        checksum_algorithm=ga.DigestType.MD5, 
+                        value=hash_values.get("MD5")
                     )
                 )
-            elif hash_type == "SHA1":
+            elif hash_type == ga.DigestType.SHA1.name:
                 self.logger.debug("Generating SHA1 digest")
-                digest = hashlib.sha1(gff_data).hexdigest()
                 checksums.append(
                     ga.Checksum(
-                        id=urn, checksum_algorithm=ga.DigestType.SHA1, value=digest
+                        id=urn,
+                        checksum_algorithm=ga.DigestType.SHA1, 
+                        value= hash_values.get("SHA1")
                     )
                 )
             else:
@@ -731,30 +862,20 @@ class Gff3:
 ##ARGUEMENTS##
 # Argument #1: The URL of the GFF file
 @click.argument("content_url", type=str)
-# Argument #2: The taxon ID of the organism
-@click.argument("taxon_id", type=str)
-# Argument #3: The ID of the genome assembly
-@click.argument("assembly_id", type=str)
-# Argument #4: The version of the genome assembly
-@click.argument("assembly_version", type=str)
-# Argument #5: The label of the genome assembly
-@click.argument("assembly_label", type=str)
-# Argument #6: The label of the genome
-@click.argument("genome_label", type=str)
-# Argument #7: The version of the genome
-@click.argument("genome_version", type=str)
-# Argument #8: The authority responsible for the genome
-@click.argument("genome_authority", type=str)
-##OPTIONS##
-@click.option("--hash_function", "-h", required=False, multiple=True, type=str, default=DEFAULT_HASH, help="A list of hash functions to use for generating checksums. Defaults to ('SHA256', 'MD5').")
-@click.option("--assembly_strain", "-s", required=False, default=None, help="The strain of the genome assembly. Defaults to None.")
 
-def cli(content_url, taxon_id, assembly_id, assembly_version, assembly_label, genome_label, genome_version, genome_authority, hash_function, assembly_strain, **args):
+##OPTIONS##
+# Option #1: The ID of the genome assembly
+@click.option("assembly_accession", "-a", required=False, default = None, type=str)
+# Option #2: The strain of the genome assembly
+@click.option("--assembly_strain", "-s", required=False, default=None, type=str, help="The strain of the genome assembly. Defaults to None.")
+# Option #3: A list of hash functions to use for generating checksums
+@click.option("--hash_function", "-h", required=False, multiple=True, type=str, default=DEFAULT_HASH, help="A list of hash functions to use for generating checksums. Defaults to ('SHA256', 'MD5').")
+
+def cli(content_url, assembly_accession, assembly_strain, hash_function, **args):
     hash_list = list(set(hash_function))
-    gff3 = Gff3(content_url, taxon_id, assembly_id, assembly_version, assembly_label, genome_label, genome_version, genome_authority, hash_list, assembly_strain)
+    gff3 = Gff3(content_url, assembly_accession, assembly_strain, hash_list)
     gff3.parse()
     gff3.serialize_to_jsonld()
-
 
 if __name__ == "__main__":
     cli()
