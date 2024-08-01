1dd6c5b7eb4431b40d779f48b1972de41339dba6
Puja Trivedi <xspuja@gmail.com>
2024-08-01 13:08:44 -0700

    removed __eq__ and __ne__ because they are not being used and were defined incorrectly

--- bkbit/model_editors/add_dunderMethods_genomeAnnotation.py
+++ bkbit/model_editors/add_dunderMethods_genomeAnnotation.py
@@ -6,8 +6,6 @@ with open(file_path, "r") as file:
     content = file.read()
 
 # Define the patterns to check if the functions already exist
-ne_pattern = r"def __ne__\(self, other\):"
-eq_pattern = r"def __eq__\(self, other\):"
 hash_pattern = r"def __hash__\(self\):"
 
 # Find the GeneAnnotation class
@@ -18,15 +16,9 @@ if class_match:
     class_start = class_match.end()
 
     # Check if the functions already exist
-    has_ne = re.search(ne_pattern, content[class_start:])
-    has_eq = re.search(eq_pattern, content[class_start:])
     has_hash = re.search(hash_pattern, content[class_start:])
 
     # Add the functions only if they do not exist
-    if not has_ne:
-        content = content.replace(class_match.group(), class_match.group() + "\n    def __ne__(self, other):\n        return (self.description != other.description) or (self.molecular_type != other.molecular_type)\n    ")
-    if not has_eq:
-        content = content.replace(class_match.group(), class_match.group() + "\n    def __eq__(self, other):\n        return (self.description == other.description) and (self.molecular_type == other.molecular_type)\n    ")
     if not has_hash:
         content = content.replace(class_match.group(), class_match.group() + "\n\n    def __hash__(self):\n        return hash(tuple([self.id, self.name, self.molecular_type, self.description]))\n    ")
 
