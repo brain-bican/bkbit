c1e541d9f7f0cc1a4bff9e0d9d24848024232a7b
Dorota Jarecka <djarecka@gmail.com>
2024-08-18 17:40:57 -0400

    adding yaml 2 cvs converter

--- bkbit/cli.py
+++ bkbit/cli.py
@@ -1,5 +1,6 @@
 import click
 from bkbit.model_converters.sheets_converter import schema2model
+from bkbit.model_converters.yaml2sheet_converter import yaml2cvs
 
 @click.group()
 def cli():
@@ -8,6 +9,7 @@ def cli():
 
 # Add commands to the CLI group
 cli.add_command(schema2model)
+cli.add_command(yaml2cvs)
 
 if __name__ == '__main__':
     cli()
