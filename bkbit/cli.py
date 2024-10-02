48a394e2bd51a713cfb108fc7b548b520e3940d4
Puja Trivedi <xspuja@gmail.com>
2024-10-02 14:55:29 -0700

    removed taxon files that required git lfs

--- bkbit/cli.py
+++ bkbit/cli.py
@@ -4,6 +4,7 @@ from bkbit.data_translators.library_generation_translator import specimen2jsonld
 from bkbit.model_converters.yaml2sheet_converter import yaml2cvs
 from bkbit.data_translators.file_manifest_translator import filemanifest2jsonld
 from bkbit.data_translators.genome_annotation_translator import gff2jsonld
+from bkbit.utils.get_ncbi_taxonomy import download_ncbi_taxonomy
 
 @click.group()
 def cli():
@@ -16,6 +17,7 @@ cli.add_command(specimen2jsonld)
 cli.add_command(yaml2cvs)
 cli.add_command(filemanifest2jsonld)
 cli.add_command(gff2jsonld)
+cli.add_command(download_ncbi_taxonomy)
 
 if __name__ == '__main__':
     cli()
--- bkbit/utils/load_json.py
+++ bkbit/utils/load_json.py
@@ -1,13 +1,19 @@
 import json
+
 def load_json(file_path):
     """
-    Load JSON data from a file.
+    Load a JSON file from the given file path.
 
     Args:
         file_path (str): The path to the JSON file.
 
     Returns:
-        dict: The loaded JSON data as a dictionary.
+        dict: The contents of the JSON file as a dictionary.
+
+    Raises:
+        FileNotFoundError: If the file does not exist.
+        JSONDecodeError: If the file is not a valid JSON.
+
     """
     with open(file_path, "r", encoding="utf-8") as f:
         return json.load(f)
