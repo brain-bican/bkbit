0f9971540be587046e080ce15d69e09b8674b5ce
Puja Trivedi <xspuja@gmail.com>
2024-11-06 09:15:01 -0800

    added cli function that parses large metadata file from data catalog and returns a list of all the library aliquot nhash_ids

--- bkbit/cli.py
+++ bkbit/cli.py
@@ -1,8 +1,9 @@
 import click
 from bkbit.model_converters.sheets_converter import schema2model
-from bkbit.data_translators.library_generation_translator import specimen2jsonld
 from bkbit.model_converters.yaml2sheet_converter import yaml2cvs
+from bkbit.data_translators.library_generation_translator import specimen2jsonld
 from bkbit.data_translators.file_manifest_translator import filemanifest2jsonld
+from bkbit.data_translators.specimen_metadata_translator import list_library_aliquot
 
 @click.group()
 def cli():
@@ -11,9 +12,10 @@ def cli():
 
 # Add commands to the CLI group
 cli.add_command(schema2model)
-cli.add_command(specimen2jsonld)
 cli.add_command(yaml2cvs)
+cli.add_command(specimen2jsonld)
 cli.add_command(filemanifest2jsonld)
+cli.add_command(list_library_aliquot)
 
 if __name__ == '__main__':
     cli()
---
+++ bkbit/data_translators/specimen_metadata_translator.py
@@ -0,0 +1,31 @@
+import csv
+import click
+
+def extract_specimen_ids(csv_file_path):
+    """
+    Reads a CSV file and prints the 'Specimen ID' column to the command line.
+
+    Parameters:
+    - csv_file_path: str, path to the input CSV file.
+    """
+    with open(csv_file_path, 'r', encoding='utf-8') as csv_file:
+        reader = csv.DictReader(csv_file)
+        
+        # Ensure 'Specimen ID' column exists in the CSV
+        if 'Specimen ID' not in reader.fieldnames:
+            raise ValueError("The CSV file does not contain the 'Specimen ID' column.")
+        
+        # Print 'Specimen ID' values to the command line
+        for row in reader:
+            specimen_id = row['Specimen ID']
+            if specimen_id.startswith("LA"):
+                print(specimen_id)
+
+@click.command
+@click.argument('specimen_metadata_file_path')
+
+def list_library_aliquot(specimen_metadata_file_path):
+    extract_specimen_ids(specimen_metadata_file_path)
+
+if __name__ == '__main__':  
+    list_library_aliquot()
