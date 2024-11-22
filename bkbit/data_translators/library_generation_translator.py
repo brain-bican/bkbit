5744ef2d4a99d7033b9da1136d15a3f49d12d260
Puja Trivedi <xspuja@gmail.com>
2024-11-22 12:07:10 -0800

    added NIMP prefix to NHashIDs referenced in was_derived_from

--- bkbit/data_translators/library_generation_translator.py
+++ bkbit/data_translators/library_generation_translator.py
@@ -279,9 +279,11 @@ class SpecimenPortal:
                 continue
             if nimp_field_name == "was_derived_from" and was_derived_from is not None:
                 if multivalued:
-                    assigned_attributes[schema_field_name] = was_derived_from
+                    # Prefix each string with "NIMP:" and assign the list
+                    assigned_attributes[schema_field_name] = [f"NIMP:{value}" for value in was_derived_from]
                 else:
-                    assigned_attributes[schema_field_name] = was_derived_from[0]
+                    # Prefix the single string with "NIMP:" and assign it
+                    assigned_attributes[schema_field_name] = f"NIMP:{was_derived_from[0]}"
                 continue
             data_value = data.get("record", {}).get(nimp_field_name)
             if data_value is None:
