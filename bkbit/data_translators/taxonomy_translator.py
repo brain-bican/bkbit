b1b8654153e02c5a585b6311d3009daf00f2ff75
Puja Trivedi <xspuja@gmail.com>
2025-03-25 22:29:58 -0700

    added function (_get_attributes) that returns a list of dictionaries containing field_names and their respective value from the given data (format: parquet file). This function is generalized for any class from the bke_taxonomy schema.

--- bkbit/data_translators/taxonomy_translator.py
+++ bkbit/data_translators/taxonomy_translator.py
@@ -3,9 +3,12 @@ from collections import defaultdict
 import pandas as pd
 import pyarrow.parquet as pq
 
+from bkbit.models import bke_taxonomy as bke_tax
+
 class Taxonomy:
     def __init__(self):
         self.relations = {}
+        self.display_colors = {}
 
 
     @staticmethod
@@ -30,3 +33,21 @@ class Taxonomy:
                 }
             )
         self.relations = relations
+    def parse_display_color(self,file_path):
+        """Parse display color data from a parquet file and return a dictionary of display colors."""
+        display_colors = self._get_attributes(file_path, bke_tax.DisplayColor)
+
+    @staticmethod
+    def _get_attributes(file_path, class_name):
+        """Parse attributes from a parquet file and return a dictionary of attributes."""
+        df = pd.read_parquet(file_path)
+        all_object_attributes = []
+        for _, row in df.iterrows():
+            row_attributes = {}
+            for schema_field_name, schema_field_metadata in class_name.__fields__.items():
+                data_field_name = schema_field_metadata.json_schema_extra.get("linkml_meta", {}).get("local_names", {}).get("bke", {}).get("local_name_value", schema_field_name)
+                if data_field_name in row:
+                    row_attributes[schema_field_name] = row[data_field_name]
+            all_object_attributes.append(row_attributes)
+        return all_object_attributes
+   
