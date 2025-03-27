27f8ee1cf50be723f1e88ed54d992b08497dce2a
Puja Trivedi <xspuja@gmail.com>
2025-03-26 22:47:49 -0700

    changed the process of parsing relationships.parquet. No longer creating dictionary mapping; just indexing the df based off a given subject_id and predicate.

--- bkbit/data_translators/taxonomy_translator.py
+++ bkbit/data_translators/taxonomy_translator.py
@@ -2,14 +2,17 @@ from collections import defaultdict
 
 import pandas as pd
 import pyarrow.parquet as pq
-
+from tqdm import tqdm
+from multiprocessing import Pool
 from bkbit.models import bke_taxonomy as bke_tax
+from bkbit.utils.generate_bkbit_id import generate_object_id
+
 
 class Taxonomy:
     def __init__(self):
-        self.relations = {}
-        self.display_colors = {}
-
+        self.relations = pd.DataFrame()
+        self.display_colors = []
+        self.xref_to_bkbit_id = {}
 
     @staticmethod
     def generate_cell_type_taxon(file_path):
@@ -18,24 +21,36 @@ class Taxonomy:
         df = table.to_pandas()
         return df
 
-    def read_relationships(self,file_path):
-        """Read relationship data from parquet file and store in relations dictionary."""
-        df = pd.read_parquet(file_path)
-        relations = defaultdict(list)
-        for _, row in df.iterrows():
-            subject_id = row['subject_id']
-            relations[subject_id].append(
-                {
-                    "predicate": row['predicate'],
-                    "object_id": row['object_id'],
-                    "subject_type": row['subject_type'],
-                    "object_type": row['object_type']
-                }
-            )
-        self.relations = relations
-    def parse_display_color(self,file_path):
+    @staticmethod
+    def _process_row(row):
+        relation = {
+            "predicate": row["predicate"],
+            "object_id": row["object"],
+            "subject_type": row["subject_type"],
+            "object_type": row["object_type"],
+        }
+        print(relation)
+        return relation
+
+    def read_relationships(self, file_path):
+        """Read relationship data from a parquet file and store in relations dictionary."""
+        df = pd.read_parquet(file_path, engine="pyarrow").set_index(
+            ["subject_id", "predicate"]
+        )
+        self.relations = df
+
+    def get_relations(self, subject_id, predicate):
+        """Retrieve relations for a given subject ID and predicate from the taxonomy."""
+        rows = self.relations.loc[subject_id, predicate]
+        return rows
+
+    def parse_display_color(self, file_path):
         """Parse display color data from a parquet file and return a dictionary of display colors."""
         display_colors = self._get_attributes(file_path, bke_tax.DisplayColor)
+        for display_color in display_colors:
+            display_color["id"] = generate_object_id(display_color)
+            self.display_colors.append(bke_tax.DisplayColor(**display_color))
+            self.xref_to_bkbit_id[display_color["xref"]] = display_color["id"]
 
     @staticmethod
     def _get_attributes(file_path, class_name):
@@ -44,10 +59,17 @@ class Taxonomy:
         all_object_attributes = []
         for _, row in df.iterrows():
             row_attributes = {}
-            for schema_field_name, schema_field_metadata in class_name.__fields__.items():
-                data_field_name = schema_field_metadata.json_schema_extra.get("linkml_meta", {}).get("local_names", {}).get("bke", {}).get("local_name_value", schema_field_name)
+            for (
+                schema_field_name,
+                schema_field_metadata,
+            ) in class_name.__fields__.items():
+                data_field_name = (
+                    schema_field_metadata.json_schema_extra.get("linkml_meta", {})
+                    .get("local_names", {})
+                    .get("bke", {})
+                    .get("local_name_value", schema_field_name)
+                )
                 if data_field_name in row:
                     row_attributes[schema_field_name] = row[data_field_name]
             all_object_attributes.append(row_attributes)
         return all_object_attributes
-   
